a94ec28314a6d7e742660a9c8a14a920a8a4cfaf
Angela Romo <angela_9412@hotmail.com>
2014-03-06 22:21:00 -0600

    Clases basicas agregadas (animacion, base, main, soundclip)

--- src/brbad/BrBad.java
+++ src/brbad/BrBad.java
@@ -6,18 +6,247 @@
 
 package brbad;
 
+//Importar todas las librerias a utilizar
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.Toolkit;
+
+
 /**
  *
  * @author Cesar , Angela
  * A01036009, A01139764
  */
-public class BrBad {
+public class BrBad extends JFrame implements Runnable {
+
+    // Declarar todas las variables
+    private long tiempoActual; //Variables de control de tiempo de la animacion
+    private long tiempoInicial; //Variables de control de tiempo de la animacion
+    private boolean pausa; // bandera para manejar la pausa
+    private Image dbImage;// Imagen a proyectar	
+    private Graphics dbg; // Objeto grafico
+    
+
+    //Constructor
+    public BrBad () {
+        setTitle("JFrame Breaking Blocks");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(200, 200);
+        this.setSize(800, 600); //tamaño del jframe
+    }
+    
+    /**
+     * Metodo <I>run</I> sobrescrito de la clase <code>Thread</code>.<P>
+     * En este metodo se ejecuta el hilo, es un ciclo indefinido donde se
+     * incrementa la posicion en x o y dependiendo de la direccion, finalmente
+     * se repinta el <code>JFrame</code> y luego manda a dormir el hilo.
+     *
+     */
+    public void run() {
+
+        //Guarda el tiempo actual del sistema
+        tiempoActual = System.currentTimeMillis();
+
+        //Ciclo principal del JFrame. Actualiza y despliega en pantalla hasta que se acaben las vidas
+        //while (vidas > 0) {
+
+            //si esta pausado no actualizas ni checas colision 
+            if (!pausa) {
+                actualiza();
+                checaColision();
+            }
+            repaint(); // Se actualiza el <code>JFrame</code> repintando el contenido.
+            try {
+                // El thread se duerme.
+                Thread.sleep(150);
+            } catch (InterruptedException ex) {
+                System.out.println("Error en " + ex.toString());
+            }
+
+        //}
+    }
+    
+     /**
+     * Metodo <I>actualiza</I>
+     * Es usado para actualizar la posicion de los personajes y los valores de
+     * las variables.
+     */
+    public void actualiza(){
+        //Determina el tiempo que ha transcurrido desde que el Applet inicio su ejecuciÃ³n
+        long tiempoTranscurrido = System.currentTimeMillis() - tiempoActual;
+
+        //Guarda el tiempo actual
+        tiempoActual += tiempoTranscurrido;
+        //nubecita.actualiza(tiempoActual);
+    }
+    
+    /**
+     * Metodo <I>checaColision</I>
+     * Metodo usado para checar las colisiones de los objetos barquito y rayito
+     * entre sí y con las orillas del <code>JFrame</code>.
+     */
+    public void checaColision() {
+    
+    }
+    
+    /**
+     * Metodo <I>paint</I>
+     * En este metodo lo que hace es actualizar el contenedor (Update)
+     *
+     * @param g es el <code>objeto grafico</code> usado para dibujar.
+     */
+    public void paint(Graphics g) {
+        // Inicializan el DoubleBuffer
+        if (dbImage == null) {
+            dbImage = createImage(this.getSize().width, this.getSize().height);
+            dbg = dbImage.getGraphics();
+        }
+
+        // Actualiza la imagen de fondo.
+        dbg.setColor(getBackground());
+        dbg.fillRect(0, 0, this.getSize().width, this.getSize().height);
+
+        // Actualiza el Foreground.
+        dbg.setColor(getForeground());
+        paint1(dbg);
+
+        // Dibuja la imagen actualizada
+        g.drawImage(dbImage, 0, 0, this);
+
+    }
+    
+    /**
+     * Metodo <I>paint1</I>
+     * En este metodo se dibuja la imagen con la posicion actualizada, ademas
+     * que cuando la imagen es cargada te despliega una advertencia. (Paint)
+     *
+     * @param g es el <code>objeto grafico</code> usado para dibujar.
+     */
+    public void paint1(Graphics g) {
+    
+    }
+    
+     /**
+     * Metodo <I>keyTyped</I> sobrescrito de la interface
+     * <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al presionar una tecla que
+     * no es de accion.
+     *
+     * @param e es el <code>evento</code> que se genera en al presionar las
+     * teclas.
+     */
+    public void keyTyped(KeyEvent e) {
+    }
 
     /**
-     * @param args the command line arguments
+     * Metodo <I>keyPressed</I> sobrescrito de la interface
+     * <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al presionar cualquier la
+     * tecla.
+     *
+     * @param e es el <code>evento</code> generado al presionar las teclas.
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_P) {    //Presiono letra P
+            pausa = !pausa; //cambio valor de pausa
+        }
+    /*    if (e.getKeyCode() == KeyEvent.VK_LEFT) { //Presiono flecha izquierda
+            barquito.setDireccion(1);
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) { //Presiono flecha derecha
+            barquito.setDireccion(2);
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_I) { //Presiono tecla I
+            instrucciones = !instrucciones; //quito o pongo instrucciones
+            if (instrucciones) {  // pauso cuando hay instrucciones para evitar que el juego continue mientras aparecen estas
+                pausa = true;
+            }
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_S) { // Presiono tecla S
+            sonidoActivado = !sonidoActivado; //activo o desactivo el sonido
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_G) { //Presiono tecla G
+            if (!instrucciones) {
+                guarda = true; //registro el cambio
+                auxCarga = true;
+            }
+        
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_C) { //Presiono teclo C
+            if (!instrucciones) {
+                carga = true;
+            } //registro el cambio
+
+        }
+    */
+    }
+
+    /**
+     * Metodo <I>keyReleased</I> sobrescrito de la interface
+     * <code>KeyListener</code>.<P>
+     * En este metodo maneja el evento que se genera al soltar la tecla
+     * presionada.
+     *
+     * @param e es el <code>evento</code> que se genera en al soltar las teclas.
+     */
+    public void keyReleased(KeyEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
+
+    /**
+     * Metodo mouseClicked sobrescrito de la interface MouseListener. En este
+     * metodo maneja el evento que se genera al hacer click con el mouse sobre
+     * algun componente. e es el evento generado al hacer click con el mouse.
+     */
+    public void mouseClicked(MouseEvent e) {
+      
+    }
+
+    /**
+     * Metodo mousePressed sobrescrito de la interface MouseListener. En este
+     * metodo maneja el evento que se genera al presionar un botón del mouse
+     * sobre algun componente. e es el evento generado al presionar un botón del
+     * mouse sobre algun componente.
+     */
+    public void mousePressed(MouseEvent e) {
+    }
+
+    /**
+     * Metodo mouseReleased sobrescrito de la interface MouseListener. En este
+     * metodo maneja el evento que se genera al soltar un botón del mouse sobre
+     * algun componente. e es el evento generado al soltar un botón del mouse
+     * sobre algun componente.
+     */
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    /**
+     * Metodo mouseEntered sobrescrito de la interface MouseListener. En este
+     * metodo maneja el evento que se genera cuando el mouse entra en algun
+     * componente. e es el evento generado cuando el mouse entra en algun
+     * componente.
+     */
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    /**
+     * Metodo mouseExited sobrescrito de la interface MouseListener. En este
+     * metodo maneja el evento que se genera cuando el mouse sale de algun
+     * componente. e es el evento generado cuando el mouse sale de algun
+     * componente.
+     */
+    public void mouseExited(MouseEvent e) {
     }
     
 }
---
+++ src/brbad/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package brbad;
+
+/**
+ *
+ * @author Angela Romo, Cesar Rodriguez
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Crea un nuevo objeto Principal
+        BrBad juego = new BrBad();
+        //Despliega la ventana en pantalla al hacerla visible
+        juego.setVisible(true);
+    }
+    
+}
